b48a4ec276166066e3ab8f6e394ee70b2e6d4144
Dean Adams <dcadams@iastate.edu>
2022-04-07 10:15:39 -0500

    multiPCMs

--- MultivariatePCMs/scripts/MultivariatePCMs.r
+++ MultivariatePCMs/scripts/MultivariatePCMs.r
@@ -1,40 +1,52 @@
+## ----Ordination_prep ----
+library(geomorph)
+data(plethspecies) 
+Y.gpa <- gpagen(plethspecies$land, print.progress = F)    #GPA-alignment
+
+## ----PCA_example ----
+PCA <- gm.prcomp(Y.gpa$coords, phy = plethspecies$phy)
+plot(PCA, main = "PCA", pch = 21, bg = "red")
+
+## ----Phylomorph_example ----
+plot(PCA, phylo = TRUE, pch = 21, bg = "red",
+     main = "Phylomorphospace")
+
+## ----phyloPCA_example ----
+phylo.PCA <- gm.prcomp(Y.gpa$coords, phy = plethspecies$phy, GLS = TRUE)
+plot(phylo.PCA, phylo = TRUE, pch = 21, bg="red",
+     main = "phylogenetic PCA")
+
+## ----PaCA_example ----
+PaCA.gls <- gm.prcomp(Y.gpa$coords, phy = plethspecies$phy, 
+                      align.to.phy = TRUE, GLS = TRUE)
+plot(PaCA.gls, phylo = TRUE, pch = 21, bg = "red",
+     main = "PaCA: Phylogenetically-Aligned Component Analysis")
 
 ## ----read_data ----
-library(geomorph)
+library(RRPP)
 library(geiger)
 library(phytools)
 tree.best<-read.nexus("../data/Consensus of 1000 salamander trees.nex") #Maximum Credible Tree
 plethdata<-read.csv("../data/meandata-CinGlutOnly.csv",row.names=1, header=TRUE )
-Y<-plethdata[,c(3:8)] ##This is our multivariate data
-Y<-apply(Y,2,as.numeric);row.names(Y)<-row.names(plethdata) ##This is our formated data
 svl<-plethdata[,2];names(svl)<-row.names(plethdata) 
 groups<-as.factor(plethdata[,1]); names(groups)<-row.names(plethdata)
+Y<-as.matrix(plethdata[,c(3:8)])  ##This is our multivariate data
 plethtree<-treedata(phy = tree.best,data = plethdata, warnings = FALSE)$phy
 plot(plethtree)
 axisPhylo(1)
-
-
 PCA<-gm.prcomp(Y,phy=plethtree)   #principal components of Y
 plot(PCA, pch=21,bg="black") ##we can put the PCA directly into the plot function
 
-## ----Phylomorphospace ----
-plot(PCA,phylo=TRUE)
-
-## ----phylo_pca ----
-
-phylo_PCA<-gm.prcomp(Y,phy=plethtree,GLS=T)   #principal components of Y
-plot(phylo_PCA, pch=21,bg="black") ##we can put the PCA directly into the plot function
-
 ## ----phylo_reg----
-geo_df <- geomorph.data.frame(Y=Y, svl=svl,groups=groups,phy = plethtree) #needed for geomorph
-PGLS.reg<-procD.pgls(Y ~ svl, phy = phy, data = geo_df, iter = 999, print.progress = FALSE) # randomize residuals
-summary(PGLS.reg)
-plot(PGLS.reg, type = "regression", predictor=svl,reg.type = "RegScore")
-
+PhyCov <- vcv.phylo(plethtree) #generate phylogenetic covariance matrix
+rdf <- rrpp.data.frame(Y=Y, svl=svl,groups=groups,Cov = PhyCov)  #RRPP data frame
+PGLS.reg <- lm.rrpp(Y ~ svl, Cov = PhyCov, data = rdf, iter = 999, 
+                   print.progress = FALSE,) # randomize residuals  
+anova(PGLS.reg)
 
 ## ---- phylo_ANOVA----
-PGLS.aov<-procD.pgls(Y ~ groups, phy = phy, data = geo_df, iter = 999, print.progress = FALSE) # randomize residuals
-summary(PGLS.aov)  #no difference once phylogeny considered
+PGLS.aov<-lm.rrpp(Y ~ groups, Cov = PhyCov, data = rdf, iter = 999, print.progress = FALSE) # randomize residuals
+anova(PGLS.aov)  #no difference once phylogeny considered
 
 ## ---- phylo_PLS ----
 PLS.Y <- phylo.integration(A = Y[,1:3], A2 = Y[,4:6], phy= plethtree, print.progress = FALSE)
